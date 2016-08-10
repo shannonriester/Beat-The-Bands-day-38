ad52da5be45cf0f3bd16d72730e41ba8aa870a42
Shannon Riester <shannonriester@gmail.com>
2016-08-10 16:11:18 -0500

    commented out daily challenge

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -47,4 +47,35 @@ else if (!localStorage.authtoken) {
 }
 
 
-ReactDOM.render(router, document.getElementById('container'));
+// ReactDOM.render(router, document.getElementById('container'));
+
+const Checkbox = React.createClass({
+  getInitialState: function() {
+    return {checked: false,}
+  },
+  checkHandler: function(e) {
+    if (!e.which || e.which === 13) {
+      this.setState({checked: !this.state.checked});
+    }
+  },
+  render: function() {
+    let checker;
+    let message = '"check" me out ;)!';
+    if (this.state.checked) {
+      checker = (<div className="checkbox-inner"></div>);
+      message = 'hey! you checked me out!!'
+    }
+    return (
+      <div className="daddy-container">
+        <label htmlFor="checkbox1">{message}</label>
+        <div id="checkbox1" tabIndex="0" className="checkbox-container" role="checkbox" onClick={this.checkHandler} onKeyUp={this.checkHandler}>
+          {checker}
+        </div>
+      </div>
+    );
+  }
+});
+
+
+
+ReactDOM.render(<Checkbox />, document.getElementById('container'));
